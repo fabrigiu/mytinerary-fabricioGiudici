6c482090fe640068c67c833df1cb83826ed60cd9
Fabricio Giudici <fabriciogiudici97@hotmail.com>
2023-08-10 07:58:18 -0300

    new svg icons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,116 +1,145 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
 function Carousel() {
-    const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
-    const cities = [
-        {
-            name: 'Shanghái',
-            url: 'https://cdn.britannica.com/08/187508-050-D6FB5173/Shanghai-Tower-Gensler-San-Francisco-world-Oriental-2015.jpg'
-        },
-        {
-            name: 'Kyoto',
-            url: 'https://lp-cms-production.imgix.net/2021-02/shutterstockRF_787331263.jpg'
-        },
-        {
-            name: 'Los Angeles',
-            url: 'https://a.travel-assets.com/findyours-php/viewfinder/images/res70/475000/475464-Los-Angeles.jpg'
-        },
-        {
-            name: 'Río De Janeiro',
-            url: 'https://www.civitatis.com/blog/wp-content/uploads/2022/10/panoramica-rio-janeiro-brasil.jpg'
-        },
-        {
-            name: 'Moscow',
-            url: 'https://s1.eestatic.com/2019/12/22/actualidad/actualidad_453966497_141085637_1706x960.jpg'
-        },
-        {
-            name: 'Rome',
-            url: 'https://hips.hearstapps.com/hmg-prod/images/elle-fin-de-semana-en-roma-1642526556.jpg'
-        },
-        {
-            name: 'Seoul',
-            url: 'https://viajes.nationalgeographic.com.es/medio/2021/12/23/seul_e984508d_1800x1145.jpg'
-        },
-        {
-            name: 'İstanbul',
-            url: 'https://img.itinari.com/pages/images/original/8ab02a03-8901-4117-8b2a-b9e6ffbadbcf-istock-507551802.jpg?ch=DPR&dpr=2.625&w=1200&h=800&s=fa63837a31065457f603923b764c1ab3'
-        },
-        {
-            name: 'Sídney',
-            url: 'https://viajes.nationalgeographic.com.es/medio/2017/02/13/shutterstock-358498871_b1c3e8d1.jpg'
-        },
-        {
-            name: 'Suzhou',
-            url: 'https://planetofhotels.com/guide/sites/default/files/styles/paragraph__live_banner__lb_image__1880bp/public/live_banner/suzhou-1.jpg'
-        },
-        {
-            name: 'México City',
-            url: 'https://i.natgeofe.com/n/6c02ad5a-977b-4f12-b9c0-02ffb0736e07/metropolitan-cathedral-zocalo-mexico-city.JPG'
-        },
-        {
-            name: 'Tokyo',
-            url: 'https://assets.vogue.com/photos/5e387ae1be72df00088bf8c8/master/w_2560%2Cc_limit/EEX74X.jpg'
-        }
-    ];
+  const cities = [
+    {
+      name: "Shanghái",
+      url: "https://cdn.britannica.com/08/187508-050-D6FB5173/Shanghai-Tower-Gensler-San-Francisco-world-Oriental-2015.jpg",
+    },
+    {
+      name: "Kyoto",
+      url: "https://lp-cms-production.imgix.net/2021-02/shutterstockRF_787331263.jpg",
+    },
+    {
+      name: "Los Angeles",
+      url: "https://a.travel-assets.com/findyours-php/viewfinder/images/res70/475000/475464-Los-Angeles.jpg",
+    },
+    {
+      name: "Río De Janeiro",
+      url: "https://www.civitatis.com/blog/wp-content/uploads/2022/10/panoramica-rio-janeiro-brasil.jpg",
+    },
+    {
+      name: "Moscow",
+      url: "https://s1.eestatic.com/2019/12/22/actualidad/actualidad_453966497_141085637_1706x960.jpg",
+    },
+    {
+      name: "Rome",
+      url: "https://hips.hearstapps.com/hmg-prod/images/elle-fin-de-semana-en-roma-1642526556.jpg",
+    },
+    {
+      name: "Seoul",
+      url: "https://viajes.nationalgeographic.com.es/medio/2021/12/23/seul_e984508d_1800x1145.jpg",
+    },
+    {
+      name: "İstanbul",
+      url: "https://img.itinari.com/pages/images/original/8ab02a03-8901-4117-8b2a-b9e6ffbadbcf-istock-507551802.jpg?ch=DPR&dpr=2.625&w=1200&h=800&s=fa63837a31065457f603923b764c1ab3",
+    },
+    {
+      name: "Sídney",
+      url: "https://viajes.nationalgeographic.com.es/medio/2017/02/13/shutterstock-358498871_b1c3e8d1.jpg",
+    },
+    {
+      name: "Suzhou",
+      url: "https://planetofhotels.com/guide/sites/default/files/styles/paragraph__live_banner__lb_image__1880bp/public/live_banner/suzhou-1.jpg",
+    },
+    {
+      name: "México City",
+      url: "https://i.natgeofe.com/n/6c02ad5a-977b-4f12-b9c0-02ffb0736e07/metropolitan-cathedral-zocalo-mexico-city.JPG",
+    },
+    {
+      name: "Tokyo",
+      url: "https://assets.vogue.com/photos/5e387ae1be72df00088bf8c8/master/w_2560%2Cc_limit/EEX74X.jpg",
+    },
+  ];
 
-    const startIndex = currentSlide * 4;
-    
-    
-    const visibleCities = cities.slice(startIndex, startIndex + 4);
-    
-    const previSlide = () => {
-        setCurrentSlide((prevSlideIndex) =>
-            prevSlideIndex === 0 ? Math.ceil(cities.length / 4) - 1 : prevSlideIndex - 1
-        );
-    };
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentSlide((prevSlideIndex) =>
-            prevSlideIndex === Math.ceil(cities.length / 4) - 1 ? 0 : prevSlideIndex + 1
-            );
-        }, 2000);
+  const startIndex = currentSlide * 4;
 
-        return () => {
-            clearInterval(interval);
-        };
-    }, [currentSlide]);
-    
-    const nextSlide = () => {
-        setCurrentSlide((prevSlideIndex) =>
-            prevSlideIndex === Math.ceil(cities.length / 4) - 1 ? 0 : prevSlideIndex + 1
-        );
-    };
-    
+  const visibleCities = cities.slice(startIndex, startIndex + 4);
 
-
-
-    return (
-        <div className="bg-orange-100 carousel flex flex-row justify-around items-center h-[25rem] my-4 lg:justify-evenly">
-            <button className="prev-btn" onClick={previSlide}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-[4px] lg:mr-[3rem] hover:animate-pulse">
-                    <path d="M15.75 19.5L8.25 12l7.5-7.5" />
-                </svg>
-            </button>
-            <div className="slides grid grid-cols-2 grid-rows-2 gap-6 items-center h-64 w-96 lg:h-72 lg:w-[34rem]">
-                {visibleCities.map((city, index) => (
-                    <div key={index} className="slide group h-full w-full shadow-fxc transform transition duration-300 ease-out">
-                        <p className="absolute inset-x-0 bottom-0 text-transparent duration-200 group-hover:text-gray-100 group-hover:bg-orange-500/60 ">{city.name}</p>
-                        <img
-                            className="box-content object-cover object-center h-full w-full"
-                            src={city.url}
-                            alt={city.name}
-                        />
-                    </div>
-                ))}
-            </div>
-            <button className="next-btn" onClick={nextSlide}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 ml-[4px] lg:ml-[3rem] hover:animate-pulse">
-                    <path d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                </svg>
-            </button>
-        </div>
+  const previSlide = () => {
+    setCurrentSlide((prevSlideIndex) =>
+      prevSlideIndex === 0
+        ? Math.ceil(cities.length / 4) - 1
+        : prevSlideIndex - 1
     );
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide((prevSlideIndex) =>
+        prevSlideIndex === Math.ceil(cities.length / 4) - 1
+          ? 0
+          : prevSlideIndex + 1
+      );
+    }, 2000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [currentSlide]);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlideIndex) =>
+      prevSlideIndex === Math.ceil(cities.length / 4) - 1
+        ? 0
+        : prevSlideIndex + 1
+    );
+  };
+
+  return (
+    <div className="bg-orange-100 carousel flex flex-row justify-around items-center h-[25rem] my-4 lg:justify-evenly">
+      <button className="prev-btn" onClick={previSlide}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953l7.108-4.062A1.125 1.125 0 0121 8.688v8.123zM11.25 16.811c0 .864-.933 1.405-1.683.977l-7.108-4.062a1.125 1.125 0 010-1.953L9.567 7.71a1.125 1.125 0 011.683.977v8.123z"
+          />
+        </svg>
+      </button>
+      <div className="slides grid grid-cols-2 grid-rows-2 gap-6 items-center h-64 w-96 lg:h-72 lg:w-[34rem]">
+        {visibleCities.map((city, index) => (
+          <div
+            key={index}
+            className="slide group h-full w-full shadow-fxc transform transition duration-300 ease-out"
+          >
+            <p className="absolute inset-x-0 bottom-0 text-transparent duration-200 group-hover:text-gray-100 group-hover:bg-orange-500/60 ">
+              {city.name}
+            </p>
+            <img
+              className="box-content object-cover object-center h-full w-full"
+              src={city.url}
+              alt={city.name}
+            />
+          </div>
+        ))}
+      </div>
+      <button className="next-btn" onClick={nextSlide}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M3 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062A1.125 1.125 0 013 16.81V8.688zM12.75 8.688c0-.864.933-1.405 1.683-.977l7.108 4.062a1.125 1.125 0 010 1.953l-7.108 4.062a1.125 1.125 0 01-1.683-.977V8.688z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
 }
 
-export default Carousel
+export default Carousel;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,25 @@ import Menu2 from "./Menu2";
 import Menu from "./Menu";
 
 function Header() {
-
   return (
     <header className="navbar flex justify-between items-center text-base pt-1 pb-1 w-full font-semibold font-custom">
       <h1 className="text-xl sm:text-xl">My Tinerary</h1>
       <nav className="flex justify-between items-center">
         <Menu />
-        <button className="flex items-center gap-3">
+        <button className="flex items-center gap-3 hover:animate-pulse">
           <svg
-            viewBox="0 0 900 1000"
-            fill="currentColor"
-            height="1em"
-            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
           >
-            <path d="M800 50c28 0 51.667 9.667 71 29s29 43 29 71v700c0 26.667-9.667 50-29 70s-43 30-71 30H350c-26.667 0-49.667-10-69-30s-29-43.333-29-70V750h98v100h450V150H350v150h-98V150c0-28 9.667-51.667 29-71s42.333-29 69-29h450M450 720V600H0V450h450V330l200 194-200 196" />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+            />
           </svg>
         </button>
         <Menu2 />
